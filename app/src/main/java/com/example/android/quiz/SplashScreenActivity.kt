b9ea54184b7dc9b513d121cd3fc0f4b38943efae
Peter <petermada@gmail.com>
2022-01-21 20:39:11 +0100

    Create splash screen

---
+++ app/src/main/java/com/example/android/quiz/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.android.quiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val splashIcon = findViewById<ImageView>(R.id.splash_trophy)
+        splashIcon.alpha = 0f
+        splashIcon.animate().setDuration(2000).alpha(1f).withEndAction{
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+    }
+}
